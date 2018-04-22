d9cd2f15e8e7b49fe965a2cf7396549910a9b9ab
Tonmoy Das <tonmoy1a@gmail.com>
2018-04-22 17:01:09 +0600

    insert

--- class/Database.php
+++ class/Database.php
@@ -1,25 +1,27 @@
 <?php
 
-class Database 
-{	
-	public $conn;
-	public $select='*';
-	public $where='';
-	public $param=[];
-	public $limit='';
-	public $order_by='';
+class Database
+{
+    public $conn;
+    public $select='*';
+    public $where='';
+    public $param=[];
+    public $limit='';
+    public $order_by='';
 
-	public function __construct() {
+    public function __construct()
+    {
         try {
-	    	$this->conn = new PDO('mysql:host=localhost;dbname=bitm_course', 'root', '');
-	    	$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		} catch (PDOException $e) {
-	    	echo 'ERROR: ' . $e->getMessage();
-		}
+            $this->conn = new PDO('mysql:host=localhost;dbname=bitm_course', 'root', '');
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo 'ERROR: ' . $e->getMessage();
+        }
     }
 
-	public function get($table=''){
-		try {
+    public function get($table = '')
+    {
+        try {
             $query = "SELECT $this->select FROM `$table` $this->where $this->limit";
             $stmt = $this->conn->prepare($query);
             $stmt->execute(array_values($this->param));
@@ -27,49 +29,55 @@ class Database
         } catch (Exception $ex) {
             echo $ex->getMessage();
         }
-	}
+    }
 
-	public function getAll($table=''){
-		try {
+    public function getAll($table = '')
+    {
+        try {
             $query = "SELECT $this->select FROM `$table` $this->where $this->order_by $this->limit";
             $stmt = $this->conn->prepare($query);
-			$stmt->execute(array_values($this->param));
+            $stmt->execute(array_values($this->param));
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         } catch (Exception $ex) {
             echo $ex->getMessage();
         }
-	}
+    }
 
-	public function insert($request_data=[]){
-		try {
-			$query = "";
-		} catch (PDOException $e){
-			echo $e->getMessage;
-		}
-	}
+    public function insert($table = '', $request_data = [])
+    {
+        try {
+            $query = "INSERT INTO $table";
+        } catch (PDOException $e) {
+            echo $e->getMessage;
+        }
+    }
 
-	public function select($param=[]){
-		$this->select= implode(',',$param);
-		return $this;
-	}
+    public function select($param = [])
+    {
+        $this->select= implode(',', $param);
+        return $this;
+    }
 
-	public function where($param=[]){
-		$this->param = $param;
-		$w='';
-		foreach ($param as $col => $value) {
-			$w.= $col.'= ? AND ';
-		}
-		$this->where= 'WHERE '.trim($w,'AND ');
-		return $this;
-	}
+    public function where($param = [])
+    {
+        $this->param = $param;
+        $w='';
+        foreach ($param as $col => $value) {
+            $w.= $col.'= ? AND ';
+        }
+        $this->where= 'WHERE '.trim($w, 'AND ');
+        return $this;
+    }
 
-	public function limit($offset='',$total_no=''){
-		$this->limit = 'LIMIT '.$offset.','.$total_no;
-		return $this;
-	}
+    public function limit($offset = '', $total_no = '')
+    {
+        $this->limit = 'LIMIT '.$offset.','.$total_no;
+        return $this;
+    }
 
-	public function order_by($column='', $order=''){
-		$this->order_by = 'ORDER BY '.$column.' '.$order;
-		return $this;
-	}
+    public function order_by($column = '', $order = '')
+    {
+        $this->order_by = 'ORDER BY '.$column.' '.$order;
+        return $this;
+    }
 }
