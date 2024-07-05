41170c7aff6b3f504d032e9ac66e72ebc3c0711e
Tonmoy Das <tonmoy1a@gmail.com>
2024-07-05 10:45:03 +0600

    example updated

--- example.php
+++ example.php
@@ -1,15 +1,3 @@
 <?php
 
 include 'class/Database.php';
-
-class Example extends Database
-{
-    
-
-}
-
-$obj = new Example;
-echo '<pre>';
-$b=$obj->limit(0, 5)->getAll('vw_sell_with_price');
-$b=$obj->insert('as', array('name' => 'values', ));
-//print_r($b);
